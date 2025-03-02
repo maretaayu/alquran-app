2ae63b9f900a55889044d1c5b35a2d3d768330cf
Mareta Ayu Handanari <maretahandanari@gmail.com>
2025-03-02 07:29:00 +0700

    fix(path): adjust import path

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import HomePage from "./pages/HomePage";
+import HomePage from "./pages/Homepage";
 import SurahDetail from "./pages/SurahDetail";
 import "./App.css";
 
